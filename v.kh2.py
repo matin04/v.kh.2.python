88715e86100fd690a8d6f7bfb596087933307b4e
matin04 <fathuddinzodamatin@gmail.com>
2025-05-07 18:33:12 +0300

    4

--- v.kh2.py
+++ v.kh2.py
@@ -1,10 +1,11 @@
-# thadacha 3 ////////////////
+# thadacha 4 ////////////////
 
 
-x=int(input())
-if x>1 :
+a=int(input())
+b=int(input())
+if a>b :
     print(1)
-elif x<0:
-    print(-1)
+elif a<b:
+    print(2)
 else:
     print(0)
