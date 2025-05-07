e22d882f9e7d544fd8ea0e3bf03aecdf6c98203c
matin04 <fathuddinzodamatin@gmail.com>
2025-05-07 18:49:18 +0300

    8

--- v.kh2.py
+++ v.kh2.py
@@ -1,11 +1,11 @@
-# thadacha 7 ////////////////
+# thadacha 8 ////////////////
 
 
 x1=int(input())
 x2=int(input())
 y1=int(input())
 y2=int(input())
-if abs(x1-x2)-abs(y1-y2):
+if (x1 == x2 or y1 == y2 or abs(x1 - x2) == abs(y1 - y2)):
     print("yes")
 else:
     print('no')
