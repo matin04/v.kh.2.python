86153c678a363efa8847060a68f6dc381260ea60
matin04 <fathuddinzodamatin@gmail.com>
2025-05-07 18:54:47 +0300

    10

--- v.kh2.py
+++ v.kh2.py
@@ -1,11 +1,13 @@
-# thadacha 9 ////////////////
+# thadacha 10 ////////////////
 
 
 x1=int(input())
 x2=int(input())
 y1=int(input())
 y2=int(input())
-if (abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1):
+dx = abs(x1 - x2)
+dy = abs(y1 - y2)
+if ((dx == 2 and dy == 1) or (dx == 1 and dy == 2)):
     print("yes")
 else:
     print('no')
